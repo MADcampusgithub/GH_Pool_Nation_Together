8a15367ed05ec5ed0bc4c3abdcb59cf1c9514978
MADcampusgithub <maxime.adler@campus-la-chataigneraie.org>
2021-03-08 18:48:42 +0100

    Pool_Nation_Together
    jeu de billard multi local

---
+++ Pool_Nation_Together/Assets/Scripts/MouvementBoule.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvementBoule : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f, 1000f)]
+    float force;
+    Rigidbody RD;
+
+    void Start()
+    {
+        RD = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RD.AddForce(Vector3.forward * force, ForceMode.Impulse);
+
+        }
+    }
+}
